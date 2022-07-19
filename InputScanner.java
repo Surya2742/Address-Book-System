da415bb808708b517ca47e14514a543de81da19a
Surya2742 <boby7724284@gmail.com>
2022-07-19 22:05:43 +0530

    UC1-Contacts Details Created

---
+++ InputScanner.java
@@ -0,0 +1,15 @@
+package addressBookSystem;
+
+import java.util.Scanner;
+
+public class InputScanner {
+
+	public static Scanner scanner = new Scanner(System.in);
+	public String inputString() {
+		return scanner.next();
+	}
+
+	public void scannerClose() {
+		scanner.close();
+	}
+}
